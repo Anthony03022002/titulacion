d0ebb14db3632cb430c89714e360a9cb8c91af94
metmaqluin <met.malquin@yavirac.edu.ec>
2023-12-29 14:52:52 -0500

    nueva

--- proyecto_api/settings.py
+++ proyecto_api/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pagosdb',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
