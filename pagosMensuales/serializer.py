9197b2d79ade1be7db138b5177a789cbf26ddc7a
kevin <kevinfalconi94@gmail.com>
2023-12-29 13:04:09 -0500

    tablas eliminadas

--- pagosMensuales/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import pagosMensuales
-
-class pagosMensualesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = pagosMensuales
-        # fields = ('pagos_mensuales', 'vencimiento', 'estado', 'total_cobrar', 'fecha_inicio')
-        fields = '__all__'
